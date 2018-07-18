3ccdafaafded5b05e5e7a63bc43f6e99fa56684c
lawrend <douglas_lawrence@mac.com>
2018-07-17 20:20:13 -0700

    added functional counter using redux

--- reducer.js
+++ reducer.js
@@ -1,3 +1,4 @@
+//Creates a functioning counter redux app
 let state;
 function changeState(state = {count: 0}, action){
     switch (action.type) {
@@ -7,3 +8,24 @@ function changeState(state = {count: 0}, action){
         return state;
     }
   }
+
+//adding event handling without jQuery
+function dispatch(action) {
+  state = changeState(state, action)
+  render()
+}
+
+function render() {
+  let container = document.getElementById('container')
+  container.textContent = state.count;
+}
+
+//attach dispatch execution
+let button = document.getElementById('button')
+button.addEventListener('click', function(){
+  dispatch({type: 'INCREASE_COUNT'})
+})
+
+
+
+dispatch({type: '@@INIT'})
